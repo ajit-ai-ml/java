92a6e4675e2df0b49ed6db4cbbcc807fc41593ba
Ajit Kumar <ajitk121005@gmail.com>
2026-08-20 15:55:50 +0530

    jdbc updated

--- jdbc_java/DatabaseConnection.java
+++ jdbc_java/DatabaseConnection.java
@@ -1,49 +1,52 @@
+ 
 
 import java.sql.*;
-public class DatabaseConnection {
-    private static final String  url="jdbc:mysql://localhost:3306/college"; 
-    private static final String  name="root"; 
-    private static final String  password="root";
-    
 
+public class DatabaseConnection {
+
+    private static final String url =
+            "jdbc:mysql://localhost:3306/college";
+
+    private static final String name = "root";
+    private static final String password = "root";
 
     public static void main(String[] args) {
-        try{
+
+        try {
             Class.forName("com.mysql.cj.jdbc.Driver");
 
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
-
         }
-        try{
-            Connection con = DriverManager.getConnection(url, name, password);
-             Statement statement= con.createStatement();
-             String query ="select * from student";
-             ResultSet resultSet=statement.executeQuery(query);
-              while(resultSet.next()){
-                int rollno=resultSet.getInt("rollno");
-                String name=resultSet.getString("name");
+
+        try {
+            Connection con =DriverManager.getConnection(url, name, password);
+
+            Statement statement = con.createStatement();
+
+            String query = "SELECT * FROM student";
+
+            ResultSet resultSet =statement.executeQuery(query);
+
+            while (resultSet.next()) {
+
+                int rollno = resultSet.getInt("rollno");
+                String name = resultSet.getString("name");
                 int marks = resultSet.getInt("marks");
-                String grade=resultSet.getString("grade");
+                String grade = resultSet.getString("grade");
                 String city = resultSet.getString("city");
-                System.out.println("name " + name);
-                System.out.println("rollno"+rollno);
-                System.out.println("garde: "+grade);
-                System.out.println("city: "+city);
-                System.out.println("marks: "+marks);
 
+                System.out.println("name: " + name + "rollno: "+rollno +"grade: "+grade +"city: "+city +"marks: "+marks);
+//                System.out.println("rollno: " + rollno);
+//                System.out.println("grade: " + grade);
+//                System.out.println("city: " + city);
+//                System.out.println("marks: " + marks);
+            }
 
+            con.close();
 
-              }
-             
-             
-        } catch(Exception e){
-                System.out.println(e.getMessage());
-              }
-
-
-        
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
-
-    
 }
