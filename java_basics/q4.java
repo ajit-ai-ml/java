3036e76fade1efe2a04710808eba60be34443ddb
Ajit Kumar <ajitk121005@gmail.com>
2026-08-27 00:24:42 +0530

    modified java basic

--- java_basics/q4.java
+++ java_basics/q4.java
@@ -2,7 +2,7 @@
 import java.util.Scanner;
  
 public class q4 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws  Exception{
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the id of the employee");
         int id =scanner.nextInt();
