6875920e28445fc84f68d7b6474d09b8f452a917
Ajit Kumar <ajitk121005@gmail.com>
2026-08-27 12:07:24 +0530

    q3 server client added

---
+++ java_networking/q3client.java
@@ -0,0 +1,30 @@
+package java_networking;
+import java.io.*;
+import java.net.*;
+
+public class q3client {
+    public static void main(String[] args) throws Exception{
+        String ip="localhost";
+        int port=9898;
+
+
+        Socket socket= new Socket(ip,port);
+
+        PrintWriter pw = new PrintWriter(socket.getOutputStream(),true);
+        pw.println("hello i am client!");
+
+        BufferedReader rd =new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        String servermessage=rd.readLine();
+        System.out.println("server said: "+servermessage);
+
+        String mess=rd.readLine();
+        System.out.println("srever: "+mess);
+
+        socket.close();
+        
+
+
+    }
+    
+}
---
+++ java_networking/q3server.java
@@ -0,0 +1,34 @@
+package java_networking;
+import java.net.*;
+import java.io.*;
+
+
+public class q3server {
+    public static void main(String[] args) throws Exception {
+        ServerSocket server =new ServerSocket(9898);
+        System.out.println("server is waiting for client...");
+        Socket socket=server.accept();
+        System.out.println("server is connnected succesfully");
+
+        BufferedReader br=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        String message=br.readLine();
+
+        System.out.println("client says: "+message);
+
+        PrintWriter pw =new PrintWriter(socket.getOutputStream(),true);
+        pw.println("hy client i am your server!");
+
+        OutputStreamWriter os = new OutputStreamWriter(socket.getOutputStream());
+        String mess="hy this message send using outputstreamwriter\n";
+        os.write(mess);
+        os.flush();
+
+            
+
+
+                server.close();
+        socket.close();
+        
+    }
+    
+}
