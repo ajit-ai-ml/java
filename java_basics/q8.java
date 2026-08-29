e8a9fc0abf87c142fcbc3b5d297e78a757b57ac9
Ajit Kumar <ajitk121005@gmail.com>
2026-08-29 20:37:34 +0530

    q8 modified

--- java_basics/q8.java
+++ java_basics/q8.java
@@ -1,5 +1,13 @@
 public class q8 {
     public static void main(String[] args) {
+    try{
+        int a =5;
+        int b =0;
+        int result =a/b;
+        System.out.println(result);
+    }catch(Exception e){
+        System.out.println(e.getStackTrace());
+    }
         
     }
     
